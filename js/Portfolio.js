0a5099da1b95e3b0eab60139dd045f1747f88769
DinamickaDevelopment <info@dinamicka.com>
2015-10-19 21:23:39 +0300

    add main code

---
+++ js/Portfolio.js
@@ -0,0 +1,91 @@
+﻿$(function () {
+    $("#elastic_grid_demo").elastic_grid({
+        'showAllText': 'All',
+        'filterEffect': 'popup', // moveup, scaleup, fallperspective, fly, flip, helix , popup
+        'hoverDirection': true,
+        'hoverDelay': 0,
+        'hoverInverse': false,
+        'expandingSpeed': 500,
+        'expandingHeight': 500,
+        'items':
+        [
+           
+            {
+                'title': 'Single Zoo',
+                'description': 'SingleZoo company spesialty is pet grooming and veterinarian services. Also it provides goods for pets and accommodation services for animals. SingleZoo website is developed for small business with all required features. Mobile friendly and elegant website is the best choice to bring your company to the right customers via Internet.',
+                'thumbnail': ['images/Portfolio/small/SingleZoo_250x250.jpg', 'images/Portfolio/small/SingleZoo_250x250_2.jpg', 'images/Portfolio/small/SingleZoo_250x250_3.jpg'],
+                'large': ['images/Portfolio/large/SingleZoo_500x500.jpg', 'images/Portfolio/large/SingleZoo_500x500_2.jpg', 'images/Portfolio/large/SingleZoo_500x500_3.jpg'],
+                'button_list':
+                [
+                    
+                    { 'title': 'View', 'url': 'http://singlezoo.hol.es/', 'new_window': true }
+                ],
+                'tags': ['Portrait']
+            },
+           
+            {
+                'title': 'Real estate services',
+                'description': 'Elegant and responsive design for real estate service website. Pixel - perfect development were done according to all client`s requirements. As the result there is informative and modern website.',
+                'thumbnail': ['images/Portfolio/small/AM_250x250_1.jpg', 'images/Portfolio/small/AM_250x250_2.jpg', 'images/Portfolio/small/AM_250x250_3.jpg'],
+                'large': ['images/Portfolio/large/AM_500x500_1.jpg', 'images/Portfolio/large/AM_500x500_2.jpg', 'images/Portfolio/large/AM_500x500_3.jpg'],
+                'button_list':
+                [
+                ],
+                'tags': ['Portrait']
+            },
+            {
+                'title': 'BrainStorm',
+                'description': 'Our client wanted a parallax and animation and we built a front-end page in HTML CSS, JavaScript and JQuery.',
+                'thumbnail': ['images/Portfolio/small/BrainStorm_250x250_1.jpg', 'images/Portfolio/small/BrainStorm_250x250_2.jpg', 'images/Portfolio/small/BrainStorm_250x250_3.jpg'],
+                'large': ['images/Portfolio/large/BrainStorm_500x500_1.jpg', 'images/Portfolio/large/BrainStorm_500x500_2.jpg', 'images/Portfolio/large/BrainStorm_500x500_3.jpg'],
+                'button_list':
+                [
+                    { 'title': 'View', 'url': 'http://dinamickadevelopment.github.io/Nousles/', 'new_window': true }
+                    
+                ],
+                'tags': ['Portrait']
+            },
+            {
+                'title': 'Coffee',
+                'description': 'Creative layout in vector, as a template of landing page for cafe.There was no client so we oriented just on our ideas.',
+                'thumbnail': ['images/Portfolio/small/Coffee_250x250_1.jpg', 'images/Portfolio/small/Coffee_250x250_2.jpg', 'images/Portfolio/small/Coffee_250x250_3.jpg'],
+                'large': ['images/Portfolio/large/Coffee_500x500_1.jpg', 'images/Portfolio/large/Coffee_500x500_2.jpg', 'images/Portfolio/large/Coffee_500x500_3.jpg'],
+                'button_list':
+                [                    
+                ],
+                'tags': ['Portrait']
+            },
+            {
+                'title': 'FMXA',
+                'description': 'Website for marketing agency. We have created redesign for website and branding guidelines from scratch to improve user experience, enhance usability for visual elements. I have also enlarged amount of content according to documents provided by client. Now only holding page is online, other pages in development progress.',
+                'thumbnail': ['images/Portfolio/small/FMXA_250x250_1.jpg', 'images/Portfolio/small/FMXA_250x250_2.jpg', 'images/Portfolio/small/FMXA_250x250_3.jpg'],
+                'large': ['images/Portfolio/large/FMXA_500x500_1.jpg', 'images/Portfolio/large/FMXA_500x500_2.jpg', 'images/Portfolio/large/FMXA_500x500_3.jpg'],
+                'button_list':
+                [
+                    { 'title': 'View', 'url': 'http://fmxa.co.uk/', 'new_window': true }
+                    
+                ],
+                'tags': ['Portrait']
+            },
+            {
+                'title': 'Dreams',
+                'description': "Colorful layout  was made to fulfil designer's idea and has its own plot. This design will be perfect choice for creative companies, services and goods for kids or art cafe.",
+                'thumbnail': ['images/Portfolio/small/Dreams_250x250_1.jpg', 'images/Portfolio/small/Dreams_250x250_2.jpg', 'images/Portfolio/small/Dreams_250x250_3.jpg'],
+                'large': ['images/Portfolio/large/Dreams_500x500_1.jpg', 'images/Portfolio/large/Dreams_500x500_2.jpg', 'images/Portfolio/large/Dreams_500x500_3.jpg'],
+                'button_list':
+                [                    
+                ],
+                'tags': ['Portrait']
+            },
+        ]
+    });
+
+    $("li.portrait").addClass("animated_delete_fill-mode");
+    $("li.portrait:nth-child(4n+1)").addClass(" fadeInLeft anim "); // Left 1&5
+    $("li.portrait:nth-child(4n+4)").addClass(" fadeInRight anim "); // Right 4&8
+    $("li.portrait").slice(1, 3).addClass(" fadeInDown anim "); //Down 2&3
+    $("li.portrait").slice(5, 7).addClass(" fadeInUp anim "); //Down 6&7
+    
+    
+
+});
---
+++ js/mailLogic.js
@@ -0,0 +1,39 @@
+﻿var xmlhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+xmlhttp.open('POST', 'https://mandrillapp.com/api/1.0/messages/send.json');
+xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+var Form = document.forms.contact
+function mailsend(e) {
+    e.preventDefault();
+    var
+    namee = Form.contactName.value,
+    email = Form.contactAddress.value,
+    subj = Form.contactSubject.value,
+    msg = Form.contactMessage.value,
+    sendBlock = document.getElementById('message_sent');
+
+xmlhttp.send(JSON.stringify({
+    'key': 'V0D_Zxz9tADoT1PJUBYXhQ',
+    'message': {
+        'from_email': 'Dinamicka@opinion.net',
+        'to': [{ 'email': 'info@dinamicka.com', 'type': 'to' }],
+        'autotext': 'true',
+        'subject': 'Opinion',
+        'html': '<head><style>td {padding: 3px;}</style></head><body><table border="1" style="border:1px solid black;border-collapse: collapse; overflow:auto;width:400px"><tr><td>Name</td><td>' + namee + '</td></tr><tr><td>Address</td><td>' + email + '</td></tr><tr><td>Subject</td><td>' + subj + '</td></tr><tr><td>Message</td><td>' + msg + '</td></tr></table></body>'
+
+    }
+}));
+Form.contactName.value = '';
+Form.contactAddress.value = '';
+Form.contactSubject.value = '';
+Form.contactMessage.value = '';
+sendBlock.style.display = 'block';
+//var async = setTimeout(function () {
+//    sendBlock.style.opacity = '1';
+//    var timer1 = setTimeout(function () {
+//        sendBlock.opa
+//    })
+//}, 0);
+$('#message_sent').show('slow');
+
+}
+Form.addEventListener('submit', mailsend, false);
