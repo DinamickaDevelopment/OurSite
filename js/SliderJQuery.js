5346c1ced2569b651cb166d25fadfde93f2bb745
Dinamicka Development <info@dinamicka.com>
2016-01-09 22:15:34 +0200

    1.2.1

---
+++ js/SliderJQuery.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    function play() {
+        setInterval(function () {
+            var next = $(".slideshow .active").removeClass("active").next();
+            if (!next.length) {
+                next = $(".slideshow .image:first");
+            }
+            next.addClass("active");
+        }, 5000);
+    }
+    play();
+
+});
